26fc9bb1abc233741c361c5204af0b8678aea2cb
40233742 <acespon01@qub.ac.uk>
2022-03-15 19:37:24 +0000

    Two test classes added.

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -12,6 +12,7 @@ import junit.framework.TestCase;
 public class RangeTest extends TestCase {
 	
 	private Range rangeObjectUnderTest;
+	private Range r1;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -22,6 +23,7 @@ public class RangeTest extends TestCase {
 
 	@Before
 	protected void setUp() throws Exception {
+		r1 = new Range(-1.0,1.0);
 		rangeObjectUnderTest = new Range(-1,1);
 	}
 
@@ -34,5 +36,17 @@ public class RangeTest extends TestCase {
 	public void testCentralValueShouldBeZero() {
 		assertEquals("The central value of -1 and 1 should be 0", 0, rangeObjectUnderTest.getCentralValue(),0.000000001d);
 	}
-
+	
+	@Test
+	public void testGetUpperBound() {
+		assertEquals("getUpperBound: Did not return the expected output",1.0,r1.getUpperBound(),0.000000001d);
+		
+	}
+	
+	@Test
+	public void testGetLowerBound() {
+		assertEquals("getLowerBound: Did not return the expected output",-1.0,r1.getLowerBound(),0.000000001d);
+		
+	}
 }
+
