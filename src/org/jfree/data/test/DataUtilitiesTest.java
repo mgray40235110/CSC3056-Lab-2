d2bb0e41ef2da5e388e4112d3a3f435873c360b9
Arthur Cespon <40233742@eeecs.qub.ac.uk>
2022-03-31 15:52:03 +0100

    DataUtilities test methods

--- src/org/jfree/data/test/DataUtilitiesTest.java
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -20,7 +20,10 @@ public class DataUtilitiesTest extends TestCase {
 
 	
 	private Values2D values2D;
+	private Values2D nullExample;
 	private KeyedValues values;
+
+	private static final double DELTA = 0.0000001d;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -32,10 +35,19 @@ public class DataUtilitiesTest extends TestCase {
 
 	@Before
 	protected void setUp() throws Exception {
+		//create a 2x2 KeyedValues2D instance, like {{1,2},{3,4}}.
 		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
 		values2D = testValues;
 		testValues.addValue(1, 0, 0);
-		testValues.addValue(4, 1, 0);
+		testValues.addValue(2,0,1);
+		testValues.addValue(3, 1, 0);
+		testValues.addValue(4,1,1);
+		
+		DefaultKeyedValues2D null2DValues = new DefaultKeyedValues2D();
+		nullExample = null2DValues;
+		null2DValues.addValue(null,0,0);
+		null2DValues.addValue(null,1,0);
+		
 		//in order to create a 1d keyed values object we must do this:
 		DefaultKeyedValues testValues1D = new DefaultKeyedValues();
 		values = testValues1D;
@@ -48,10 +60,56 @@ public class DataUtilitiesTest extends TestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
+	
+	@Test
+	public void testDataContainsNullValuesColumnTotal() {
+		assertEquals("Wrong sum returned, should be 0.",0,DataUtilities.calculateColumnTotal(nullExample, 0));
+	}
+	
+	@Test
+	public void testDataContainsNullValuesRowTotal() {
+		assertEquals("Wrong sum returned, should be 0.",0,DataUtilities.calculateRowTotal(nullExample, 0),DELTA);
+	}
 
 	@Test
-	public void testValidDataAndColumnTotal() {
-		assertEquals("Wrong sum returned. It should be 5.0", 5.0, DataUtilities.calculateColumnTotal(values2D, 0), 0.0000001d);
+	public void testValidDataAndColumnTotal1() {
+		assertEquals("Wrong sum returned. It should be 4.0", 4.0, DataUtilities.calculateColumnTotal(values2D, 0), 0.0000001d);
+	}
+	
+	@Test
+	public void testValidDataAndColumnTotal2() {
+		assertEquals("Wrong sum returned, it should be 6.0", 0, DataUtilities.calculateColumnTotal(values2D, -1));
+	}
+	
+	@Test
+	public void testValidDataAndOutsidePositiveIndexColumnTotal() {
+		try {
+			DataUtilities.calculateColumnTotal(values2D, 2);
+			fail("No exception thrown: expected output was a thrown exception of type: IndexOutOfBounds");
+		} catch(Exception e) {
+			assertTrue("Incorrect exception type thrown",e.getClass().equals(IndexOutOfBoundsException.class));
+		}
+	}
+	
+	@Test
+	public void testValidDataAndNegativeIndexColumnTotal() {
+		try {
+			DataUtilities.calculateColumnTotal(values2D, -1);
+			fail("No exception thrown: expected output was a thrown exception of type: IndexOutOfBoundsException");
+		} catch(Exception e) {
+			assertTrue("Incorrect exception type thrown",e.getClass().equals(IndexOutOfBoundsException.class));
+		}
+	}
+	
+	@Test
+	public void testNullDataValidColumnTotal() {
+		try {
+			DataUtilities.calculateColumnTotal(null, 0);
+			fail("No exception thrown, expected outcome was: a thrown exception of type: IllegalArgumentException");
+		} catch(Exception e) {
+			//System.out.println(e.getClass());
+			assertTrue("Incorrect exception type thrown",e.getClass().equals(IllegalArgumentException.class));
+		}
 	}
 	
 	@Test
@@ -85,7 +143,7 @@ public class DataUtilitiesTest extends TestCase {
 		testCase.addValue("0",-5);
 		testCase.addValue("1",- 9);
 		testCase.addValue("2",-2);
-		System.out.println(DataUtilities.getCumulativePercentages(testCase).getValue("0"));
+		//System.out.println(DataUtilities.getCumulativePercentages(testCase).getValue("0"));
 		assertEquals("Wrong KeyedValues instance returned.",testCase,DataUtilities.getCumulativePercentages(values));
 	}
 	
@@ -93,8 +151,6 @@ public class DataUtilitiesTest extends TestCase {
 	public void testCreateNumberArray() {
 		Number[] z = {1.0,2.0,3.0,4.0};
 		double[] x = {1.0,2.0,3.0,4.0};
-		System.out.println(x);
-		System.out.println(DataUtilities.createNumberArray(x).equals(z));
 		assertEquals("createNumberArray: The expected output is not correct.",z.getClass(),DataUtilities.createNumberArray(x).getClass());
 	}
 	
@@ -105,7 +161,6 @@ public class DataUtilitiesTest extends TestCase {
 			fail("No exception thrown, expected outcome was: InvalidParameterException");
 		}
 		catch (Exception e) {
-			System.out.println(e.getClass());
 			assertTrue("Incorrect exception type thrown",e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
@@ -114,6 +169,7 @@ public class DataUtilitiesTest extends TestCase {
 	public void testCreateNumberArrayTypicalValues() {
 		double[] t1 = {1.0,2.0,3.0};
 		Number[] z = {1.0,2.0,3.0};
+		assertEquals("createNumberArray",z[2],DataUtilities.createNumberArray(t1)[2]);
 		assertEquals("createaNumberArray",z.length,DataUtilities.createNumberArray(t1).length);
 	}
 	
